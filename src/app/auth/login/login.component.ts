ba11fb6a3595ff27d52841e40d89a9cd4bd4893c
duckbuddyy <emirhansoylu98@gmail.com>
2021-03-18 21:16:18 +0300

    Lecture 122

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+  private authStatusSubscription: Subscription;
   isLoading = false;
-  email: string = '';
-  password: string = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(public authService: AuthService) {
+    this.authStatusSubscription = new Subscription();
+  }
+
+  ngOnInit(): void {
+    this.authStatusSubscription.unsubscribe();
+    this.authStatusSubscription = this.authService
+      .getAuthStatusListener()
+      .subscribe((authStatus) => {
+        this.isLoading = false;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.authStatusSubscription.unsubscribe();
+  }
 
   onLogin(form: NgForm) {
     if (form.invalid) {
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit, OnDestroy {
+  private authStatusSubscription: Subscription;
   isLoading = false;
-  email: string = '';
-  password: string = '';
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) {
+    this.authStatusSubscription = new Subscription();
+  }
+
+  ngOnInit(): void {
+    this.authStatusSubscription.unsubscribe();
+    this.authStatusSubscription = this.authService
+      .getAuthStatusListener()
+      .subscribe((authStatus) => {
+        this.isLoading = false;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.authStatusSubscription.unsubscribe();
+  }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
